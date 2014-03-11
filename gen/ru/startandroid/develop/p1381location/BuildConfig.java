6bd0094da80d6f29dc20a0519a20850cc043d44f
AlexPozdnyakov <alxp.1993@gmail.com>
2014-03-11 14:01:06 +0300

    first
    first

---
+++ gen/ru/startandroid/develop/p1381location/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ru.startandroid.develop.p1381location;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
